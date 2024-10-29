04a63e4ff6d54d21ce70a856c39a10f99d79c979
Finidi47 <finidisaid@gmail.com>
2024-10-29 09:53:07 +0300

    A commit for lesson 8

---
+++ lesson8.py
@@ -0,0 +1,11 @@
+#Dictionaries
+
+# student = {"name":"Jane Sarah"  "id": '1234', "age": 21, "gender": "Female"}
+#
+# print(student["name"])
+# print(student)
+
+#set ---only one existence per item //unordered //immutable
+
+people = {"jane", "Bill", "Kevo", "Said", "Jane"}
+print(people)
