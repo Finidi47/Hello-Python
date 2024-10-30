91517ee8f6e7e75b638380b9e79829ae9479be45
Finidi47 <finidisaid@gmail.com>
2024-10-30 08:46:56 +0300

    Modify lesson 9

--- lesson9.py
+++ lesson9.py
@@ -2,3 +2,10 @@
 while True:
     print("Hello, loops")
     break
+
+# for each loop
+scores = [90, 76, 56, 81, 99, 78, 54, 34, 78, 65, 45, 23, 56, 89, 89, 45, 67, 77, 76]
+
+for score in scores:
+    if score >= 50:
+        print(score)
