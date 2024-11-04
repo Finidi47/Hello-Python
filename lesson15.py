ee658963e41bf027c56296ea6638d686f8ea257a
Finidi47 <finidisaid@gmail.com>
2024-11-04 08:53:51 +0300

    Child classes

--- lesson15.py
+++ lesson15.py
@@ -11,3 +11,21 @@ class Employee:
     def print_details(self):
         print(f"Name: {self.name} ID:{self.id_number} DOB:{self.dob} Gender:{self.gender}")
 
+class PermanentEmployee(Employee):
+    def __init__(self, name, id_number, dob, gender, salary):
+        super().__init__(name, id_number, dob, gender)
+        self.salary = salary
+    def print_salary(self):
+        print(f"Salary is {self.salary}")
+
+class TemporaryEmployee(Employee):
+    def __init__(self, name, id_number, dob, gender,hourly_pay, end_date):
+        super().__init__(name, id_number, dob, gender)
+        self.hourly_pay = hourly_pay
+        self.end_date = end_date
+
+    def print_hourly_pay(self):
+        print(f"Hourly rate is {self.hourly_pay}")
+
+    def end_date(self):
+        print(f"End date is {self.end_date}")
