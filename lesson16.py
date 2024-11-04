df30ae7010b2e53b27668c5df6c02a8b1309749c
Finidi47 <finidisaid@gmail.com>
2024-11-04 09:23:01 +0300

    Print some numbers

---
+++ lesson16.py
@@ -0,0 +1,12 @@
+# Error handling
+
+x = input("Your first number: ")
+y = input("Your second number: ")
+z = input("Your third number: ")
+
+x_num = int(x)
+y_num = int(y)
+z_num = int(z)
+
+print(x_num * y_num * z_num)
+
