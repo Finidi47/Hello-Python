0e7ea8247ac363cb0054222c788fa3542471bda3
Finidi47 <finidisaid@gmail.com>
2024-11-04 09:46:44 +0300

    40 days from today

--- lesson17.py
+++ lesson17.py
@@ -1,8 +1,11 @@
 # dates
-from datetime import date
+from datetime import date, timedelta
 
 today = date.today()
 print(today)
 
 formatted_date = today.strftime("%d/%m/%Y")
 print(formatted_date)
+
+after_forty_days  = today + timedelta(days=40)
+print(after_forty_days)
