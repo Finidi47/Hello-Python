a59e52ff31796ad2f1aa9fcb9d87adf5da12adb9
Finidi47 <finidisaid@gmail.com>
2024-11-04 10:00:41 +0300

    Calculate age of a person

--- lesson17.py
+++ lesson17.py
@@ -1,5 +1,5 @@
 # dates
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 today = date.today()
 print(today)
@@ -9,3 +9,9 @@ print(formatted_date)
 
 after_forty_days  = today + timedelta(days=40)
 print(after_forty_days)
+
+dob = '1990-03-27'
+# convert to date object
+date_of_birth = datetime.strptime(dob, "%Y-%m-%d")
+age = today.year - date_of_birth.year
+print("I'm ", age, "years old")
