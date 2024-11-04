9a7bc5f9217811703d409efac8af808003c661ef
Finidi47 <finidisaid@gmail.com>
2024-11-04 09:29:04 +0300

    Handle errors in program

--- lesson16.py
+++ lesson16.py
@@ -4,9 +4,11 @@ x = input("Your first number: ")
 y = input("Your second number: ")
 z = input("Your third number: ")
 
-x_num = int(x)
-y_num = int(y)
-z_num = int(z)
-
-print(x_num * y_num * z_num)
+try:
+    x_num = int(x)
+    y_num = int(y)
+    z_num = int(z)
+    print(x_num * y_num * z_num)
+except:
+    print("Enter valid numbers")
 
